6b638b67ad70908450dfedcc945d083581be2977
kim5257app <kim5257.app@gmai.com>
2020-10-11 17:54:50 +0900

    1. 붙필요한 파일 제거

--- dist/commRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const debug_1 = require("debug");
-const express = require("express");
-const debug = debug_1.default('polling-comm/commRouter');
-const router = express.Router();
-exports.default = router;
